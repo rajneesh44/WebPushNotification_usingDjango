ed2d9286453060bc57ef3546765e3b1ed573782d
rajneesh44 <rajneeshsharma338@gmail.com>
2020-01-23 13:45:40 +0530

    first changes

---
+++ pushnotify/views.py
@@ -0,0 +1,36 @@
+from django.http.response import HttpResponse, JsonResponse
+from django.views.decorators.http import require_GET, require_POST
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+from webpush import send_user_notification
+import json
+
+
+@require_GET
+def home(request):
+    return HttpResponse('<H1>Home Page</H1>')
+
+
+@require_POST
+@csrf_exempt
+def send_push(request):
+    try:
+        body = request.body
+        data = json.loads(body)
+
+        if 'head' not in data or 'body' not in data or 'id' not in data:
+            return JsonResponse(status=400, data={"message": "Invalid data format"})
+
+        user_id = data['id']
+        user = get_object_or_404(User, pk=user_id)
+        payload = {'head': data['head'], 'body': data['body']}
+        send_user_notification(user=user, payload=payload, ttl=1000)
+
+        return JsonResponse(status=200, data={"message": "Web push successful"})
+
+    except TypeError:
+        return JsonResponse(status=500, data={"message": "An error occurred"})
+
+
+
